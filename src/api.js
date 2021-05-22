469066996e3b706776d1c870c17c3eb9e89cdb13
marco corapi <marco.corapi@outlook.it>
2021-05-23 00:12:46 +0200

    api

--- src/api.js
+++ src/api.js
@@ -23,20 +23,22 @@ const getCurrentDay = () => {
   }
 }
 // years/months/days
+const today = new Date();
 const currentYear = new Date().getFullYear();
 const currentMonth = getCurrentMonth();
-const currentDay = getCurrentDay();
+const currentDay = today.getDate();
 const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
 const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
 const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
 
 // fetching games from the last year to the current date, ordered by rating (ascending order), 10 per page
 const popular_games = `${base_url}/games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
-export const popularGamesUrl = () => `${popular_games}`;
-// console.log(popularGamesUrl()); // use: node src/api.js and remote the export because gives error
 
+ const popularGamesUrl = () => `${popular_games}`;
+// console.log(popularGamesUrl()); // use: node src/api.js and remote the export because gives error
+console.log(popularGamesUrl());
 const upcoming_games = `games?dates=${currentDate},${nextYear}&ordering=-added&page_size=10`;
-export const upcomingGamesURL = () => `${base_url}${upcoming_games}`;
+ const upcomingGamesURL = () => `${base_url}/${upcoming_games}`;
 
 const newGames = `games?dates=${lastYear},${currentDate}&ordering=-released&page_size=10`
-export const newGamesURL = () => `${base_url}${newGames}`;
+ const newGamesURL = () => `${base_url}/${newGames}`;
--- src/reducers/gamesReducer.js
+++ src/reducers/gamesReducer.js
@@ -8,9 +8,14 @@ const initialState = {
 const gamesReducer = (state = initialState, action) => {
   switch (action.type) {
     case "FETCH_GAMES":
-      return {...state, popular: action.payload.popular}  
+      return {
+        ...state,
+        popular: action.payload.popular,
+        upcoming: action.payload.upcoming,
+        newGames: action.payload.newGames,
+      };
     default:
-      return { ...state};  
+      return { ...state };
   }
 };
 
