1b97e316eaefdb34405de87aa70bf5c8fa68e8ca
marco corapi <marco.corapi@outlook.it>
2021-05-23 16:15:23 +0200

    fixing api

--- src/api.js
+++ src/api.js
@@ -1,44 +1,43 @@
+const APIkeyNotSecureOnPurpose = "729b1b58fa974da98f79e99de0c84b5d";
+const key_url = `${APIkeyNotSecureOnPurpose}`;
 // Base URL, every request I make will have this base URL: https://rawg.io/apidocs check here
-const base_url = "https://api.rawg.io/api";
+const base_url = "https://api.rawg.io/api/";
 
 // URL with popular games, but firsr we need to set the current date
 // months
 const getCurrentMonth = () => {
-  const month = new Date().getMonth() +1;
-  console.log(month) // here month without + 1 is the previous one, not sure why
+  const month = new Date().getMonth() + 1;
+  console.log(month); // here month without + 1 is the previous one, not sure why
   if (month < 10) {
-    return `0${month}`
+    return `0${month}`;
   } else {
-    return month; 
+    return month;
   }
-}
+};
 // days
 const getCurrentDay = () => {
   const day = new Date().getDate();
-  console.log(day)
+  console.log(day);
   if (day < 10) {
-    return `0${day}`
+    return `0${day}`;
   } else {
-    return day; 
+    return day;
   }
-}
+};
 // years/months/days
-const today = new Date();
 const currentYear = new Date().getFullYear();
 const currentMonth = getCurrentMonth();
-const currentDay = today.getDate();
+const currentDay = getCurrentDay();
 const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
 const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
 const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
 
 // fetching games from the last year to the current date, ordered by rating (ascending order), 10 per page
-const popular_games = `${base_url}/games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+const popular_games = `games?key=${key_url}&dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+const upcoming_games = `games?key=${key_url}&dates=${currentDate},${nextYear}&ordering=-added&page_size=10`;
+const newGames = `games?key=${key_url}&dates=${lastYear},${currentDate}&ordering=-released&page_size=10`;
 
- const popularGamesUrl = () => `${popular_games}`;
+export const popularGamesUrl = () => `${base_url}${popular_games}`;
 // console.log(popularGamesUrl()); // use: node src/api.js and remote the export because gives error
-console.log(popularGamesUrl());
-const upcoming_games = `games?dates=${currentDate},${nextYear}&ordering=-added&page_size=10`;
- const upcomingGamesURL = () => `${base_url}/${upcoming_games}`;
-
-const newGames = `games?dates=${lastYear},${currentDate}&ordering=-released&page_size=10`
- const newGamesURL = () => `${base_url}/${newGames}`;
+export const upcomingGamesURL = () => `${base_url}${upcoming_games}`;
+export const newGamesURL = () => `${base_url}${newGames}`;
